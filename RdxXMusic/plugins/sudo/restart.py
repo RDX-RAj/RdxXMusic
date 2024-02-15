68cb9ae8d7193b0064109c60a43b9b9c6dfec890
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-15 23:59:52 +0530

    Update restart.py

--- RdxXMusic/plugins/sudo/restart.py
+++ RdxXMusic/plugins/sudo/restart.py
@@ -18,7 +18,7 @@ from RdxXMusic.utils.database import (
     remove_active_video_chat,
 )
 from RdxXMusic.utils.decorators.language import language
-from RdxXMusic.utils.pastebin import AnonyBin
+from RdxXMusic.utils.pastebin import RdxBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
