e35ccec1f840a8a053dc13d75a1ed5acd62b5484
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-16 16:52:41 +0530

    Update play.py

--- RdxXMusic/utils/inline/play.py
+++ RdxXMusic/utils/inline/play.py
@@ -67,18 +67,17 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="❀", url="https://t.me/+m4oVCt2zFhYyMTdl"),
-            InlineKeyboardButton(text="❀", url="https://t.me/+okCO84m2hZxjYTE9"),
-            InlineKeyboardButton(text="𓆩🖤𓆪", user_id="1777270311"),
-            InlineKeyboardButton(text="❀", url="https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw"),
-            InlineKeyboardButton(text="❀", url="https://t.me/+KRt7ApiJ5Ek2ZjY1"),
+            InlineKeyboardButton(text="💜", url="https://t.me/+m4oVCt2zFhYyMTdl"),
+            InlineKeyboardButton(text="💛", url="https://t.me/+okCO84m2hZxjYTE9"),
+            InlineKeyboardButton(text="🖤", user_id="1777270311"),
+            InlineKeyboardButton(text="💚", url="https://www.youtube.com/channel/UCoOmopJ8YVYz9Lm8iHhNYMw"),
         ],
         [
             InlineKeyboardButton(
-                text="˹💙˼", url="https://t.me/+RObRa7kXPIJmMjU1"
+                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+RObRa7kXPIJmMjU1"
             ),
             InlineKeyboardButton(
-                text="˹💜˼", url="https://t.me/+PtOLQT04ocMzOTJl"
+                text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+PtOLQT04ocMzOTJl"
             ),
         ],
     ]
