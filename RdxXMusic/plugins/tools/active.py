8381ffc241a82d794f41f0b425eccedc3fb6b4bf
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-16 14:16:44 +0530

    Add files via upload

--- RdxXMusic/plugins/tools/active.py
+++ RdxXMusic/plugins/tools/active.py
@@ -12,7 +12,7 @@ from RdxXMusic.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
+@app.on_message(filters.command(["activevc", "activevoice"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_chats()
@@ -44,7 +44,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
+@app.on_message(filters.command(["activev", "activevideo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
     served_chats = await get_active_video_chats()
---
+++ RdxXMusic/plugins/tools/telegraph.py
@@ -0,0 +1,32 @@
+from telegraph import upload_file
+from pyrogram import filters
+from RdxXMusic import app
+from pyrogram.types import InputMediaPhoto
+
+
+@app.on_message(filters.command(["tgm" , "link"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("๏ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
+
+        i.edit(f'๏ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➛ {url}')
+
+########____________________________________________________________######
+
+@app.on_message(filters.command(["graph" , "grf"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("๏ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
+
+        i.edit(f'๏ ʏᴏᴜʀ ɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➛ {url}')
+
