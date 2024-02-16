6280422dd72e5988031979a972d676b43216afd6
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-16 22:20:26 +0530

    Add files via upload

--- RdxXMusic/utils/inline/help.py
+++ RdxXMusic/utils/inline/help.py
@@ -132,14 +132,14 @@ def help_pannel(_, START: Union[bool, int] = None):
                
             ],
             [
-                InlineKeyboardButton(text="˹s๏ᴜʀᴄє˼", url=config.UPSTREAM_REPO),
-                InlineKeyboardButton(text="˹๏ᴡɴєʀ˼",user_id="1777270311"
-                ),
+                InlineKeyboardButton(
+                    text="˹s๏ᴜʀᴄє˼", url=config.UPSTREAM_REPO),
+                InlineKeyboardButton(
+                    text="˹๏ᴡɴєʀ˼",
+                    user_id="1777270311"),
                 InlineKeyboardButton(
                     text="˹ᴀʟʟ-в๏тѕ˼",
-                    url="https://t.me/+KRt7ApiJ5Ek2ZjY1"
-                ),
-               
+                    url="https://t.me/+KRt7ApiJ5Ek2ZjY1"),            
             ],
             mark,
         ]
