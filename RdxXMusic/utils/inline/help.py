41200dd991c75bbfde32dcac2db4cec74adc70a6
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-16 21:56:41 +0530

    Update help.py

--- RdxXMusic/utils/inline/help.py
+++ RdxXMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from RDXMUSIC import app
+from RdxXMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
