f973fc49f8c96c87ed4f37e7d507951f3128607f
VDj RDX RAj <159524633+RDX-RAj@users.noreply.github.com>
2024-02-16 14:06:32 +0530

    Add files via upload

---
+++ RdxXMusic/plugins/bot/repo.py
@@ -0,0 +1,37 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from RdxXMusic import app
+from config import BOT_USERNAME
+
+start_txt = """**
+✦ ʜᴇʏ ᴛʜᴇʀᴇ, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ᴜʜʜ !
+
+✦ ɪ ᴀᴍ ๛ʀ ᴅ x ༗
+
+✦ ᴛʜɪs ɪs ʀᴅx ᴍᴜsɪᴄ ʙᴏᴛ ᴏғғɪᴄɪᴀʟ ʀᴇᴘᴏ.
+
+✦ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ๛ᴀ ʟ ᴇ x ᴀ ༗ ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.
+**"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/+RObRa7kXPIJmMjU1"),
+          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/RDX-RAj/RdxXMusic"),
+          ],
+    ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://graph.org/file/33bc093c89898dcc318ae.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
